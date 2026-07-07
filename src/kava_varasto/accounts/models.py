f83e344471d1d220c9066e913827d37b7e4517ba
Aki Tuomi <aki.tuomi@open-xchange.com>
2026-07-07 11:18:15 +0000

    accounts: add must_change_password field to User

--- src/kava_varasto/accounts/models.py
+++ src/kava_varasto/accounts/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+    must_change_password = models.BooleanField(
+        _("must change password"),
+        default=False,
+        help_text=_(
+            "Forces the user to change their password on next login. Set "
+            "automatically when an admin creates the account or resets its password."
+        ),
+    )
