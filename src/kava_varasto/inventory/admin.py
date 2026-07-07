48adbbdc60c3dda696da465744399495080722f0
Aki Tuomi <aki.tuomi@open-xchange.com>
2026-07-07 08:33:12 +0000

    Expose broken/available quantity in equipment admin

--- src/kava_varasto/inventory/admin.py
+++ src/kava_varasto/inventory/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Category, Equipment
 
@@ -11,6 +12,19 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Equipment)
 class EquipmentAdmin(admin.ModelAdmin):
-    list_display = ["short_code", "name", "category", "quantity", "is_external_loanable"]
+    list_display = [
+        "short_code",
+        "name",
+        "category",
+        "quantity",
+        "broken_quantity",
+        "available_quantity",
+        "is_external_loanable",
+    ]
     list_filter = ["category", "is_external_loanable"]
     search_fields = ["name", "short_code"]
+    list_editable = ["broken_quantity"]
+
+    @admin.display(description=_("available quantity"))
+    def available_quantity(self, obj):
+        return obj.available_quantity
