6b33b3a583036f40e34eef32bca167796061872e
Aki Tuomi <aki.tuomi@open-xchange.com>
2026-07-08 07:43:24 +0000

    loans: add loan detail endpoint

--- src/kava_varasto/loans/urls.py
+++ src/kava_varasto/loans/urls.py
@@ -7,5 +7,6 @@ app_name = "loans"
 urlpatterns = [
     path("", views.LoanListCreateView.as_view(), name="loan-list-create"),
     path("loanable-equipment/", views.LoanableEquipmentListView.as_view(), name="loanable-equipment"),
+    path("<int:pk>/", views.LoanDetailView.as_view(), name="loan-detail"),
     path("<int:pk>/return/", views.LoanReturnView.as_view(), name="loan-return"),
 ]
